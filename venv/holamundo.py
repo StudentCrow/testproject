741d3dd9df37ed60503ea97510cf045811a83c14
Daniel Vallejos <daniel.gabriel.vallejos@estudiantat.upc.edu>
2023-04-22 12:37:34 +0200

    Test 1

--- venv/holamundo.py
+++ venv/holamundo.py
@@ -1,9 +1,21 @@
-print("Hello World!!")
-print("Bye World!")
- def consciencia():
-     """
-     >>> consciencia(1)
-     El toño
-     """
-     cuadro = " "
-     picture = "boolean"
+def persona(i):
+    """
+    >>> persona(1)
+    'El toño'
+    >>> persona(2)
+    'La kimberly'
+    >>> persona(3)
+    'El yoni'
+    """
+    nombre = " "
+    if i==1:
+        nombre = "El toño"
+    elif i==2:
+        nombre = "La kimberly"
+    elif i==3:
+        nombre = "El yoni"
+    return nombre
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
